701a13ccf687a66a0b2f62c48cf1c7b195eaaec3
Anthony Lam <al3101@nyu.edu>
2018-05-06 00:17:51 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  while integer > 0 
+    puts "#{integer} SECONDS!" 
+  end 
+  "HAPPY NEW YEAR!" 
 end
+
+
+def countdown_with_sleep(integer) 
+  
